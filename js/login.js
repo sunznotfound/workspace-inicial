f8b48261b9ce46b66c292e9ca0cce6364e196cde
douxblood <jisnetsant@gmail.com>
2024-08-27 22:10:21 -0300

    prueba del LOGIN

--- js/login.js
+++ js/login.js
@@ -1,16 +1,23 @@
 document.addEventListener("DOMContentLoaded", function(){
-        document.getElementById("loginForm").addEventListener("submit", function(event) {
-            event.preventDefault();
-            
-            let usuario = document.getElementById('username').value.trim();
-            let contrasena = document.getElementById('password').value.trim();
-
-            if (usuario === "" || contrasena === "") {
-                alert("Los campos no pueden estar vacíos.");
-            } else {
-                    sessionStorage.setItem("user", usuario);
-                    sessionStorage.setItem("isLoggedIn", true);
-                    window.location.replace("index.html");
-            }
-        });
-});
+    // Verificar si el usuario ya ha iniciado sesión
+    if (sessionStorage.getItem("isLoggedIn") === "true") {
+      // Redirigir a index.html si ya ha iniciado sesión
+      window.location.replace("index.html");
+    } else {
+      // Mostrar el formulario de login si no ha iniciado sesión
+      document.getElementById("loginForm").addEventListener("submit", function(event) {
+        event.preventDefault();
+        
+        let usuario = document.getElementById('username').value.trim();
+        let contrasena = document.getElementById('password').value.trim();
+  
+        if (usuario === "" || contrasena === "") {
+          alert("Los campos no pueden estar vacíos.");
+        } else {
+          sessionStorage.setItem("user", usuario);
+          sessionStorage.setItem("isLoggedIn", true);
+          window.location.replace("index.html");
+        }
+      });
+    }
+  });

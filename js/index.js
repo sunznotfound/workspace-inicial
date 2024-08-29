7260263a5f0586692b961079b129e8b50bd7e526
Sof <sofiarameau02@gmail.com>
2024-08-29 20:28:03 -0300

    desafiate 2 hecho

--- js/index.js
+++ js/index.js
@@ -1,14 +1,22 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function() {
+    // Mostrar el usuario en la página principal
+    let usuario = localStorage.getItem("correoUsuario");
+    if (usuario) {
+        document.getElementById("usuarioDisplay").textContent = usuario;
+    }
+
     document.getElementById("autos").addEventListener("click", function() {
         localStorage.setItem("catID", 101);
-        window.location = "products.html"
+        window.location = "products.html";
     });
+
     document.getElementById("juguetes").addEventListener("click", function() {
         localStorage.setItem("catID", 102);
-        window.location = "products.html"
+        window.location = "products.html";
     });
+
     document.getElementById("muebles").addEventListener("click", function() {
         localStorage.setItem("catID", 103);
-        window.location = "products.html"
+        window.location = "products.html";
     });
 });
--- js/login.js
+++ js/login.js
@@ -1,23 +1,26 @@
-document.addEventListener("DOMContentLoaded", function(){
-    // Verificar si el usuario ya ha iniciado sesión
-    if (sessionStorage.getItem("isLoggedIn") === "true") {
+document.addEventListener("DOMContentLoaded", function() {
+  // Verificar si el usuario ya ha iniciado sesión
+  if (sessionStorage.getItem("isLoggedIn") === "true") {
       // Redirigir a index.html si ya ha iniciado sesión
       window.location.replace("index.html");
-    } else {
+  } else {
       // Mostrar el formulario de login si no ha iniciado sesión
       document.getElementById("loginForm").addEventListener("submit", function(event) {
-        event.preventDefault();
-        
-        let usuario = document.getElementById('username').value.trim();
-        let contrasena = document.getElementById('password').value.trim();
-  
-        if (usuario === "" || contrasena === "") {
-          alert("Los campos no pueden estar vacíos.");
-        } else {
-          sessionStorage.setItem("user", usuario);
-          sessionStorage.setItem("isLoggedIn", true);
-          window.location.replace("index.html");
-        }
+          event.preventDefault();
+          
+          let usuario = document.getElementById('username').value.trim();
+          let contrasena = document.getElementById('password').value.trim();
+
+          if (usuario === "" || contrasena === "") {
+              alert("Los campos no pueden estar vacíos.");
+          } else {
+              // Guardar el usuario en localStorage
+              localStorage.setItem("correoUsuario", usuario);
+
+              sessionStorage.setItem("user", usuario);
+              sessionStorage.setItem("isLoggedIn", true);
+              window.location.replace("index.html");
+          }
       });
-    }
-  });
+  }
+});

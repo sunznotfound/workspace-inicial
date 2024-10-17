ff29e26621303f94d164c1f800562843c3913fbd
Sof <sofiarameau02@gmail.com>
2024-10-16 23:16:23 -0300

    cambios desafiate

--- js/login.js
+++ js/login.js
@@ -1,26 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {
-  // Verificar si el usuario ya ha iniciado sesión
-  if (sessionStorage.getItem("isLoggedIn") === "true") {
-      // Redirigir a index.html si ya ha iniciado sesión
-      window.location.replace("index.html");
-  } else {
-      // Mostrar el formulario de login si no ha iniciado sesión
-      document.getElementById("loginForm").addEventListener("submit", function(event) {
-          event.preventDefault();
-          
-          let usuario = document.getElementById('username').value.trim();
-          let contrasena = document.getElementById('password').value.trim();
+    // Verificar si el usuario ya ha iniciado sesión
+    if (sessionStorage.getItem("isLoggedIn") === "true") {
+        // Redirigir a index.html si ya ha iniciado sesión
+        window.location.replace("index.html");
+    } else {
+        // Mostrar el formulario de login si no ha iniciado sesión
+        document.getElementById("loginForm").addEventListener("submit", function(event) {
+            event.preventDefault();
+            
+            let usuario = document.getElementById('username').value.trim();
+            let contrasena = document.getElementById('password').value.trim();
 
-          if (usuario === "" || contrasena === "") {
-              alert("Los campos no pueden estar vacíos.");
-          } else {
-              // Guardar el usuario en localStorage
-              localStorage.setItem("correoUsuario", usuario);
+            if (usuario === "" || contrasena === "") {
+                alert("Los campos no pueden estar vacíos.");
+            } else {
+                // Guardar el usuario en localStorage (si lo necesitas)
+                localStorage.setItem("correoUsuario", usuario);
 
-              sessionStorage.setItem("user", usuario);
-              sessionStorage.setItem("isLoggedIn", true);
-              window.location.replace("index.html");
-          }
-      });
-  }
+                // Guardar el estado de sesión en sessionStorage
+                sessionStorage.setItem("user", usuario);
+                sessionStorage.setItem("isLoggedIn", "true"); // Asegúrate que sea una cadena
+                window.location.replace("index.html");
+            }
+        });
+    }
 });
